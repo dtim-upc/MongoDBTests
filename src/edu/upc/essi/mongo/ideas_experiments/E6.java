334fe888ad0ae06214aa583b57e7e5a7467765d1
Moditha <moditha@essi.upc.edu>
2020-03-06 16:58:17 +0100

    update to E6

--- src/edu/upc/essi/mongo/ideas_experiments/E6.java
+++ src/edu/upc/essi/mongo/ideas_experiments/E6.java
@@ -1,5 +1,6 @@
 package edu.upc.essi.mongo.ideas_experiments;
 
+import com.google.common.collect.Lists;
 import com.opencsv.CSVWriter;
 import edu.upc.essi.mongo.datagen.DocumentSet;
 import edu.upc.essi.mongo.datagen.Generator;
@@ -10,9 +11,13 @@ import edu.upc.essi.mongo.manager.JSONSchema;
 import org.bson.Document;
 
 import javax.json.JsonObject;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.util.StringJoiner;
 
 /**
  * Experiment 4: The goal of this experiment is to evaluate the impact of
@@ -24,47 +29,70 @@ public class E6 {
 		Generator gen = Generator.getInstance();
 
 		// 100i+1 number of attributes
-		for (int i = 0; i <= 15; ++i) {
-			int attributes = 100*i+1;
-			//We reuse e2 template generator
+//		for (int i = 0; i <= 7; ++i) {
+//		int attributes = 100 * i + 1;
+		for (int i : Lists.newArrayList(1, 2, 4, 8, 16, 32, 64, 128, 256)) {
+			int attributes = i;
+
+			// We reuse e2 template generator
 			JsonObject template = E2.generateTemplate(1, 1, true, attributes);
 			File fileForTemplate = File.createTempFile("template-", ".tmp");
 			fileForTemplate.deleteOnExit();
 			Files.write(fileForTemplate.toPath(), template.toString().getBytes());
 
-			JsonObject mongoDB_JSONSchema = JSONSchema.generateJSONSchema(template,true,true);
-			JsonObject PSQL_JSONSchema = JSONSchema.generateJSONSchema(template,true,false);
-			for (int j = 0; j < 1; ++j) {
-				gen.generateFromPseudoJSONSchema(10, fileForTemplate.getAbsolutePath()).stream()
+			JsonObject mongoDB_JSONSchema = JSONSchema.generateJSONSchema(template, true, true);
+			JsonObject PSQL_JSONSchema = JSONSchema.generateJSONSchema(template, true, false);
+			for (int j = 0; j < 200; ++j) {
+				gen.generateFromPseudoJSONSchema(50000, fileForTemplate.getAbsolutePath()).stream()
 						.map(d -> Document.parse(d.toString())).forEach(DocumentSet.getInstance().documents::add);
-				E4_MongoDBManager.getInstance("e6_"+i, attributes, mongoDB_JSONSchema, writer).insert("_JSON_withoutVal");
-				E4_MongoDBManager.getInstance("e6_"+i, attributes, mongoDB_JSONSchema, writer).insert("_JSON_withVal");
-				E6_PostgreSQLManager.getInstance("e6_"+i, attributes, PSQL_JSONSchema, writer).insert("_TUPLE");
-				E6_PostgreSQLManager.getInstance("e6_"+i, attributes, PSQL_JSONSchema, writer).insert("_JSON_withoutVal");
-				E6_PostgreSQLManager.getInstance("e6_"+i, attributes, PSQL_JSONSchema, writer).insert("_JSON_withVal");
+				E4_MongoDBManager.getInstance("e6_" + i, attributes, mongoDB_JSONSchema, writer)
+						.insert("_JSON_withoutVal");
+				E4_MongoDBManager.getInstance("e6_" + i, attributes, mongoDB_JSONSchema, writer)
+						.insert("_JSON_withVal");
+				E6_PostgreSQLManager.getInstance("e6_" + i, attributes, PSQL_JSONSchema, writer).insert("_TUPLE");
+				E6_PostgreSQLManager.getInstance("e6_" + i, attributes, PSQL_JSONSchema, writer)
+						.insert("_JSON_withoutVal");
+				E6_PostgreSQLManager.getInstance("e6_" + i, attributes, PSQL_JSONSchema, writer)
+						.insert("_JSON_withVal");
 				DocumentSet.getInstance().documents.clear();
 			}
-			E4_MongoDBManager.getInstance("e6_"+i, attributes, mongoDB_JSONSchema, writer).sum("_JSON_withoutVal");
-			E4_MongoDBManager.getInstance("e6_"+i, attributes, mongoDB_JSONSchema, writer).sum("_JSON_withVal");
-			E6_PostgreSQLManager.getInstance("e6_"+i, attributes, PSQL_JSONSchema, writer).sum("_TUPLE");
-			E6_PostgreSQLManager.getInstance("e6_"+i, attributes, PSQL_JSONSchema, writer).sum("_JSON_withoutVal");
-			E6_PostgreSQLManager.getInstance("e6_"+i, attributes, PSQL_JSONSchema, writer).sum("_JSON_withVal");
 
-			E4_MongoDBManager.getInstance("e6_"+i, attributes, mongoDB_JSONSchema, writer).size("_JSON_withoutVal");
-			E4_MongoDBManager.getInstance("e6_"+i, attributes, mongoDB_JSONSchema, writer).size("_JSON_withVal");
-			E6_PostgreSQLManager.getInstance("e6_"+i, attributes, PSQL_JSONSchema, writer).size("_TUPLE");
-			E6_PostgreSQLManager.getInstance("e6_"+i, attributes, PSQL_JSONSchema, writer).size("_JSON_withoutVal");
-			E6_PostgreSQLManager.getInstance("e6_"+i, attributes, PSQL_JSONSchema, writer).size("_JSON_withVal");
+			
+			for (int j = 0; j < 50; j++) {
 
-			E4_MongoDBManager.getInstance("e6_"+i, attributes, mongoDB_JSONSchema, writer).destroyme();
-			E6_PostgreSQLManager.getInstance("e6_"+i, attributes, PSQL_JSONSchema, writer).destroyme();
+				ProcessBuilder p21 = new ProcessBuilder("/root/mongo/distrib/clear.sh");
+				Process p31 = p21.start();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(p31.getInputStream()));
+				StringJoiner sj = new StringJoiner(System.getProperty("line.separator"));
+				reader.lines().iterator().forEachRemaining(sj::add);
+				String xresult = sj.toString();
+				int retvalx = p31.waitFor();
+				System.out.println(xresult);
+				
+				E4_MongoDBManager.getInstance("e6_" + i, attributes, mongoDB_JSONSchema, writer)
+						.sum("_JSON_withoutVal");
+				E4_MongoDBManager.getInstance("e6_" + i, attributes, mongoDB_JSONSchema, writer).sum("_JSON_withVal");
+				E6_PostgreSQLManager.getInstance("e6_" + i, attributes, PSQL_JSONSchema, writer).sum("_TUPLE");
+				E6_PostgreSQLManager.getInstance("e6_" + i, attributes, PSQL_JSONSchema, writer)
+						.sum("_JSON_withoutVal");
+				E6_PostgreSQLManager.getInstance("e6_" + i, attributes, PSQL_JSONSchema, writer).sum("_JSON_withVal");
+
+			}
+			E4_MongoDBManager.getInstance("e6_" + i, attributes, mongoDB_JSONSchema, writer).size("_JSON_withoutVal");
+			E4_MongoDBManager.getInstance("e6_" + i, attributes, mongoDB_JSONSchema, writer).size("_JSON_withVal");
+			E6_PostgreSQLManager.getInstance("e6_" + i, attributes, PSQL_JSONSchema, writer).size("_TUPLE");
+			E6_PostgreSQLManager.getInstance("e6_" + i, attributes, PSQL_JSONSchema, writer).size("_JSON_withoutVal");
+			E6_PostgreSQLManager.getInstance("e6_" + i, attributes, PSQL_JSONSchema, writer).size("_JSON_withVal");
+
+			E4_MongoDBManager.getInstance("e6_" + i, attributes, mongoDB_JSONSchema, writer).destroyme();
+			E6_PostgreSQLManager.getInstance("e6_" + i, attributes, PSQL_JSONSchema, writer).destroyme();
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
 		CSVWriter writer = new CSVWriter(new FileWriter("ideas_e6.csv"));
-		writer.writeNext(new String[] { "DB", "operation", "storage", "attributes", "runtime (ns)", "size",
-				"compresed" });
+		writer.writeNext(
+				new String[] { "DB", "operation", "storage", "attributes", "runtime (ns)", "size", "compresed" });
 		generate(writer);
 		writer.close();
 	}
--- src/edu/upc/essi/mongo/manager/E6_PostgreSQLManager.java
+++ src/edu/upc/essi/mongo/manager/E6_PostgreSQLManager.java
@@ -17,13 +17,15 @@ public class E6_PostgreSQLManager {
 	private static JsonObject JSONSchema;
 	private Connection JDBC;
 
-	public static E6_PostgreSQLManager getInstance(String table, int attributes, JsonObject JSONSchema, CSVWriter writer) throws Exception {
+	public static E6_PostgreSQLManager getInstance(String table, int attributes, JsonObject JSONSchema,
+			CSVWriter writer) throws Exception {
 		if (instance == null)
 			instance = new E6_PostgreSQLManager(table, attributes, JSONSchema, writer);
 		return instance;
 	}
 
-	public E6_PostgreSQLManager(String table, int attributes, JsonObject JSONSchema, CSVWriter writer2) throws Exception {
+	public E6_PostgreSQLManager(String table, int attributes, JsonObject JSONSchema, CSVWriter writer2)
+			throws Exception {
 		this.table = table;
 		this.attributes = attributes;
 		this.JSONSchema = JSONSchema;
@@ -32,30 +34,35 @@ public class E6_PostgreSQLManager {
 		Class.forName("org.postgresql.Driver");
 		// Drop and create DB
 
-		DriverManager.getConnection("jdbc:postgresql://localhost/", "postgres", "postgres").createStatement().execute(""
-				+ "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'ideas_experiments' AND pid <> pg_backend_pid(); "
-				+ "drop database if exists ideas_experiments; " + "create database ideas_experiments;");
-		JDBC = DriverManager.getConnection("jdbc:postgresql://localhost/ideas_experiments", "postgres", "postgres");
-
-//		DriverManager.getConnection("jdbc:postgresql://10.55.0.32/", "postgres", "user").createStatement().execute(""
+//		DriverManager.getConnection("jdbc:postgresql://localhost/", "postgres", "postgres").createStatement().execute(""
 //				+ "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'ideas_experiments' AND pid <> pg_backend_pid(); "
 //				+ "drop database if exists ideas_experiments; " + "create database ideas_experiments;");
+//		JDBC = DriverManager.getConnection("jdbc:postgresql://localhost/ideas_experiments", "postgres", "postgres");
 
-//		JDBC = DriverManager.getConnection("jdbc:postgresql://10.55.0.32/ideas_experiments", "postgres", "user");
+		DriverManager.getConnection("jdbc:postgresql://10.55.0.32/", "postgres", "user").createStatement().execute(""
+				+ "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'ideas_experiments' AND pid <> pg_backend_pid(); "
+				+ "drop database if exists ideas_experiments; " + "create database ideas_experiments;");
+		JDBC = DriverManager.getConnection("jdbc:postgresql://10.55.0.32/ideas_experiments", "postgres", "user");
+		
 		JDBC.setAutoCommit(false);
-		JDBC.createStatement().execute("CREATE EXTENSION \"postgres-json-schema\"");JDBC.commit();
+		JDBC.createStatement().execute("CREATE EXTENSION \"postgres-json-schema\"");
+		JDBC.commit();
 
 		JDBC.createStatement().execute("CREATE TABLE " + table + "_JSON_withVal (ID CHAR(24), JSON JSONB)");
-		JDBC.createStatement().execute("ALTER TABLE "+table+"_JSON_withVal ADD CONSTRAINT data_is_valid CHECK (validate_json_schema('"+JSONSchema.toString()+"', JSON))");
+		JDBC.createStatement()
+				.execute("ALTER TABLE " + table
+						+ "_JSON_withVal ADD CONSTRAINT data_is_valid CHECK (validate_json_schema('"
+						+ JSONSchema.toString() + "', JSON))");
 		JDBC.createStatement().execute("CREATE TABLE " + table + "_JSON_withoutVal (ID CHAR(24), JSON JSONB)");
-		JDBC.createStatement().execute("CREATE TABLE " + table + "_TUPLE (ID CHAR(24),"+
-				JSONSchema.getJsonArray("required").
-						stream().map(t -> t.toString()+" int").collect(Collectors.joining(","))+")");
+		JDBC.createStatement()
+				.execute("CREATE TABLE " + table + "_TUPLE (ID CHAR(24)," + JSONSchema.getJsonArray("required").stream()
+						.map(t -> t.toString() + " int").collect(Collectors.joining(",")) + ")");
 		JSONSchema.getJsonObject("properties").keySet().forEach(k -> {
 			int min = JSONSchema.getJsonObject("properties").getJsonObject(k).getInt("minimum");
 			int max = JSONSchema.getJsonObject("properties").getJsonObject(k).getInt("maximum");
 			try {
-				JDBC.createStatement().execute("ALTER TABLE "+table + "_TUPLE ADD CONSTRAINT validate"+k+" CHECK ("+k+" BETWEEN "+min+" AND "+max+")");
+				JDBC.createStatement().execute("ALTER TABLE " + table + "_TUPLE ADD CONSTRAINT validate" + k
+						+ " CHECK (" + k + " BETWEEN " + min + " AND " + max + ")");
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
@@ -79,25 +86,28 @@ public class E6_PostgreSQLManager {
 				}
 			});
 		} else {
-			DocumentSet.getInstance().documents.stream().map(d -> "INSERT INTO " + table + kind + " (ID,"+
-					d.keySet().stream().filter(k->!k.equals("_id")).sorted().collect(Collectors.joining(","))
-					+") VALUES ('" + d.get("_id") + "',"+
-					d.keySet().stream().filter(k->!k.equals("_id")).sorted().map(k->String.valueOf(d.getInteger(k))).collect(Collectors.joining(","))
-					+")").forEach(s -> {
-				try {
-					statement.addBatch(s);
-				} catch (SQLException exc) {
-					exc.printStackTrace();
-				}
-			});
+			DocumentSet.getInstance().documents.stream()
+					.map(d -> "INSERT INTO " + table + kind + " (ID," + d.keySet().stream()
+							.filter(k -> !k.equals("_id")).sorted().collect(Collectors.joining(",")) + ") VALUES ('"
+							+ d.get("_id") + "',"
+							+ d.keySet().stream().filter(k -> !k.equals("_id")).sorted()
+									.map(k -> String.valueOf(d.getInteger(k))).collect(Collectors.joining(","))
+							+ ")")
+					.forEach(s -> {
+						try {
+							statement.addBatch(s);
+						} catch (SQLException exc) {
+							exc.printStackTrace();
+						}
+					});
 
 		}
 		long startTime = System.nanoTime();
 		statement.executeBatch();
 		JDBC.commit();
 		long elapsedTime = System.nanoTime() - startTime;
-		writer.writeNext(new String[] { "Postgres", "insert", table + kind,
-				String.valueOf(attributes), String.valueOf(elapsedTime)});
+		writer.writeNext(new String[] { "Postgres", "insert", kind.substring(kind.lastIndexOf("_") + 1),
+				String.valueOf(attributes), String.valueOf(elapsedTime) });
 	}
 
 	public void sum(String kind) throws Exception {
@@ -114,8 +124,8 @@ public class E6_PostgreSQLManager {
 		rs.next();
 		System.out.println(rs.getInt(1));
 		long elapsedTime = System.nanoTime() - startTime;
-		writer.writeNext(new String[] { "Postgres", "sum", table + kind, String.valueOf(attributes),
-				String.valueOf(elapsedTime) });
+		writer.writeNext(new String[] { "Postgres", "sum", kind.substring(kind.lastIndexOf("_") + 1),
+				String.valueOf(attributes), String.valueOf(elapsedTime) });
 	}
 
 	public void size(String kind) throws SQLException {
@@ -124,8 +134,8 @@ public class E6_PostgreSQLManager {
 		PreparedStatement stmt = JDBC.prepareStatement(sql);
 		ResultSet rs = stmt.executeQuery();
 		rs.next();
-		writer.writeNext(new String[] { "Postgres", "size", table+kind, String.valueOf(attributes),
-				"", rs.getString(1) });
+		writer.writeNext(new String[] { "Postgres", "size", kind.substring(kind.lastIndexOf("_") + 1),
+				String.valueOf(attributes), "", rs.getString(1) });
 	}
 
 	public void destroyme() {
